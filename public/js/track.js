e07e3bdf8c39268c8f47b1ed0d00aec988dc4ea9
Bachar Wehbi <me@bachwehbi.net>
2017-02-01 00:34:30 +0100

    update js code

--- public/js/track.js
+++ public/js/track.js
@@ -6,66 +6,66 @@ var opts = {
 
 function init () {
 
-	var tracker = null
+  var tracker = null
 
   var bbt = new BBT('YOUR_API_KEY', {
     auth_endpoint: '/auth'
   })
 
   bbt.subscribe({
-    channel: 'private-Car', // replace this by your channel
-    resource: 'Location', // replace this by your channel GPS resource
+    channel: 'private-mychannel', // replace this by your channel
+    resource: 'location', // replace this by your channel GPS resource
     read: false, // No need to subscribe to data we are publishing
     write: true // We need to publish data
   }, function(message) {})
 
-	function success (position) {
+  function success (position) {
 
-	  var latitude = position.coords.latitude
-	  var longitude = position.coords.longitude
+    var latitude = position.coords.latitude
+    var longitude = position.coords.longitude
 
     console.log('publishing position: ', latitude, longitude)
-		bbt.publish({
-	    channel: 'private-Car',
-	    resource: 'Location'
-	  }, {
-	    'latitude': latitude,
-	    'longitude': longitude
-	  })
-	}
+    bbt.publish({
+      channel: 'private-mychannel',
+      resource: 'location'
+    }, {
+      'latitude': latitude,
+      'longitude': longitude
+    })
+  }
 
-	function error (error) {
-	  var errors = {
-	    1: 'Permission denied',
-	    2: 'Position unavailable',
-	    3: 'Request timeout'
-	  }
+  function error (error) {
+    var errors = {
+      1: 'Permission denied',
+      2: 'Position unavailable',
+      3: 'Request timeout'
+    }
 
-		alert('Error: ' + errors[error.code])
-	}
+    alert('Error: ' + errors[error.code])
+  }
 
-	function startTracking () {
+  function startTracking () {
 
-	  if (navigator.geolocation) {
-	    tracker = navigator.geolocation.watchPosition(
-	      success,
-	      error,
-				opts
-	    )
-	  } else {
-	    alert('Geolocation is not supported by this browser')
-	  }
-	}
+    if (navigator.geolocation) {
+      tracker = navigator.geolocation.watchPosition(
+        success,
+        error,
+        opts
+      )
+    } else {
+      alert('Geolocation is not supported by this browser')
+    }
+  }
 
-	function stopTracking () {
-		if (tracker != null) {
-			navigator.geolocation.clearWatch(tracker)
-			tracker = null
-		}
-	}
+  function stopTracking () {
+    if (tracker != null) {
+      navigator.geolocation.clearWatch(tracker)
+      tracker = null
+    }
+  }
 
-	return {
-		startTracking: startTracking,
-		stopTracking: stopTracking
-	}
+  return {
+    startTracking: startTracking,
+    stopTracking: stopTracking
+  }
 }
